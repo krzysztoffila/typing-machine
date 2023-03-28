3bb62359f6cabb5e17fb29d20e86d630a4fb244f
krzysztoffila <umowy.fila@gmail.com>
2023-03-28 19:47:42 +0200

    create addLetter function

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const spnText = document.querySelector('.text');
 const txt = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla facere odio, perspiciatis deleniti quidem error. Voluptatum, nesciunt veniam corrupti modi deserunt ipsa, est quasi veritatis placeat quas eligendi molestias voluptate.';
 
-let number = 0;
+let indexText = 0;
 
 const addLetter = () => {
-
-    number++;
-    console.log(`${number}`);
-    if (number == 50) clearInterval(indexTyping);
+    spnText.textContent += txt[indexText];
+    indexText++
+    if (indexText == txt.length) clearInterval(indexTyping);
+    // console.log('Koniec')
 }
 const indexTyping = setInterval(addLetter, 50);
