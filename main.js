3160d2e377435aad79a5d5550e4ffa8b5919fbc7
krzysztoffila <umowy.fila@gmail.com>
2023-03-28 19:49:49 +0200

    create time to be more flexible

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const spnText = document.querySelector('.text');
 const txt = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla facere odio, perspiciatis deleniti quidem error. Voluptatum, nesciunt veniam corrupti modi deserunt ipsa, est quasi veritatis placeat quas eligendi molestias voluptate.';
 
 let indexText = 0;
+const time = 50;
 
 const addLetter = () => {
     spnText.textContent += txt[indexText];
     indexText++
     if (indexText == txt.length) clearInterval(indexTyping);
-    // console.log('Koniec')
 }
-const indexTyping = setInterval(addLetter, 50);
+const indexTyping = setInterval(addLetter, time);
