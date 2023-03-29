4d7b0ca1015802638ceb2d4632e8bca4477c1634
krzysztoffila <umowy.fila@gmail.com>
2023-03-29 10:40:29 +0200

    Add cursorAnimation function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor')
 const txt = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla facere odio, perspiciatis deleniti quidem error. Voluptatum, nesciunt veniam corrupti modi deserunt ipsa, est quasi veritatis placeat quas eligendi molestias voluptate.';
 
 let indexText = 0;
@@ -10,3 +11,10 @@ const addLetter = () => {
     if (indexText == txt.length) clearInterval(indexTyping);
 }
 const indexTyping = setInterval(addLetter, time);
+
+const cursorTime = 500
+const cursorAnimation = () => {
+    spnCursor.classList.toggle("active")
+}
+
+setInterval(cursorAnimation, cursorTime)
